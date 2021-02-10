b24a1a5c07504427b25725af7c695d4d5aa1246a
JeranDev <jeranjb@gmail.com>
2021-02-10 11:53:13 -0700

    remove width state

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -11,7 +11,6 @@ import ScrollTop from '../components/ScrollTop'
 
 const HomeScreen = () => {
   //Hide Scrollbar
-  const [width, setWidth] = useState(window.innerWidth)
   const [height, setHeight] = useState(window.innerHeight)
 
   window.addEventListener('resize', () => {
