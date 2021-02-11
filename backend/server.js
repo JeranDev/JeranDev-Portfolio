958f78cd30879c2d6b97c7502cca33c0a4a0bd45
JeranDev <jeranjb@gmail.com>
2021-02-11 12:15:09 -0700

    Add https redirect

--- backend/server.js
+++ backend/server.js
@@ -11,9 +11,13 @@ const __dirname = path.resolve()
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '/frontend/build')))
 
-  app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
-  )
+  app.get('*', (req, res, next) => {
+    if ('https' !== req.headers['x-forwarded-proto']) {
+      res.redirect('https://' + req.hostname + req.url)
+    } else {
+      res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+    }
+  })
 } else {
   app.get('/', (req, res) => {
     res.send('API is running...')
